bc9babf1eaace7b38070b23324c97b348b451334
wow9144 <suhoo.kim2@gmail.com>
2020-07-19 22:11:38 +0900

    first commit

---
+++ precache-manifest.ea5297b2437d6aa19e630e6f7b5778cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35435c8b9ef651cfa008a458e9ec08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0c1df349ef9569ddbb7",
+    "url": "/static/css/main.d375591a.chunk.css"
+  },
+  {
+    "revision": "df2d814b24e28358226a",
+    "url": "/static/js/2.f611d064.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f611d064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c1df349ef9569ddbb7",
+    "url": "/static/js/main.8e2b0bc2.chunk.js"
+  },
+  {
+    "revision": "da3780b6ce882148660f",
+    "url": "/static/js/runtime-main.6119beeb.js"
+  }
+]);
